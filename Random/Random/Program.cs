094be4b170a1a0d6ff4f171b47fa1f99ed9e2531
kamolbekb <118444680+kamolbekb@users.noreply.github.com>
2023-07-08 18:55:43 +0500

    The program was successfully upgraded

--- Random/Random/Program.cs
+++ Random/Random/Program.cs
@@ -6,35 +6,33 @@
         {
             int random_number = new Random().Next(1, 100);
             int ans, attempts = 1;
-        qayta:
-            Console.Write("To identofy random number enter number from 1 to 100: ");
-            ans = Convert.ToInt32(Console.ReadLine());
-            if (ans < 0 && ans > 101)
+            
+            while (true)
             {
-                Console.WriteLine("Kiritilgan raqam talablarga javob bermaydi");
-                attempts++;
-                goto qayta;
-            }
-            else if (ans < random_number)
-            {
-                Console.WriteLine("The number that you entered is less than random nuber, please try again!");
-                attempts++;
-                goto qayta;
-            }
-            else if (ans > random_number)
-            {
-                Console.WriteLine("The number that you entered is greater than random nuber, please try again!");
-                attempts++;
-                goto qayta;
-            }
-            else if (ans == random_number)
-            {
-                Console.WriteLine("Congratulations!!! You have successfully find random number!");
-                Console.WriteLine($"To find random number you`ve spent {attempts}-attempts");
-            }
-            else
-            {
-                goto qayta;
+                Console.Write("To identofy random number enter number from 1 to 100: ");
+                ans = Convert.ToInt32(Console.ReadLine());
+                if (ans < 0 && ans > 101)
+                {
+                    Console.WriteLine("The number that you`ve entered doesn`t satisfy the requirements, please try again!");
+                    attempts++;
+                }
+                else if (ans < random_number)
+                {
+                    Console.WriteLine("The number that you entered is less than random number, please try again!");
+                    attempts++;
+                }
+                else if (ans > random_number)
+                {
+                    Console.WriteLine("The number that you entered is greater than random number, please try again!");
+                    attempts++;
+                }
+                else 
+                {
+                    Console.WriteLine("Congratulations!!! You have successfully find random number!");
+                    Console.WriteLine($"To find random number you`ve spent {attempts}-attempts.");
+                    break;
+                }
+               
             }
         }
     }
